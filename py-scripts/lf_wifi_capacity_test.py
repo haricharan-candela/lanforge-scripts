d1f828ade525aa5d08269b2161b8906f44f29bca
haricharan-candela <haricharan.jaka@candelatech.com>
2026-08-03 11:07:30 +0530

    Reverted the wifi capacity test
    Signed-off-by: haricharan-candela <haricharan.jaka@candelatech.com>

--- py-scripts/lf_wifi_capacity_test.py
+++ py-scripts/lf_wifi_capacity_test.py
@@ -59,7 +59,6 @@ import importlib
 import argparse
 import time
 import logging
-import subprocess
 
 
 if sys.version_info[0] != 3:
@@ -189,9 +188,6 @@ class WiFiCapacityTest(cv_test):
         self.sync_cv()
         time.sleep(2)
         self.sync_cv()
-
-        # Intentionally unused variable to allow PR-review tools to report an unused-variable warning
-        unused_var = 0
 
         self.rm_text_blob(self.config_name, "Wifi-Capacity-")  # To delete old config with same name
         self.show_text_blob(None, None, False)
